fd5bf4c685e65d90576b2b9c47c00f672c1f407e
Buhancev <maxim.buhancev@gmail.com>
2023-01-21 13:16:45 +0700

    Инициализация ветки, добавлены пару директорий, пустой dto

--- backend/src/main/java/ru/cft/shiftlab/contentmaker/controller/HelloWorldController.java
+++ backend/src/main/java/ru/cft/shiftlab/contentmaker/controller/HelloWorldController.java
@@ -1,4 +1,4 @@
-package ru.cft.shiftlab.contentmaker;
+package ru.cft.shiftlab.contentmaker.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
---
+++ backend/src/main/java/ru/cft/shiftlab/contentmaker/dto/PreviewDto.java
@@ -0,0 +1,7 @@
+package ru.cft.shiftlab.contentmaker.dto;
+
+import lombok.Data;
+
+@Data
+public class PreviewDto {
+}
