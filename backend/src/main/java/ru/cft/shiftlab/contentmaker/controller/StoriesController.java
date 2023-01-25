e7fa3b6d1428fb88b253868588ec07a5784dd415
iseeyouwatching <leonidshibanovv@gmail.com>
2023-01-25 10:43:07 +0700

    Рефакторинг кода
    - изменение названия интерфейса, предназначенного для сохранения файлов в определенную директорию;
    - добавление дефолтной реализации метода в интерфейсе
    - добавление javadoc для метода сервиса, предназначенного для сохранения JSON файла и картинки в определенную директорию
    - изменение пути у PostMapping'а в контроллере

--- backend/src/main/java/ru/cft/shiftlab/contentmaker/controller/StoriesController.java
+++ backend/src/main/java/ru/cft/shiftlab/contentmaker/controller/StoriesController.java
@@ -8,7 +8,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.cft.shiftlab.contentmaker.dto.StoriesRequestDto;
-import ru.cft.shiftlab.contentmaker.services.implementations.StoriesService;
+import ru.cft.shiftlab.contentmaker.services.implementations.JsonAndImageSaverService;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/stories")
@@ -16,13 +18,13 @@ import ru.cft.shiftlab.contentmaker.services.implementations.StoriesService;
 @RequiredArgsConstructor
 public class StoriesController {
 
-    private final StoriesService storiesService;
+    private final JsonAndImageSaverService storiesService;
 
-    @PostMapping("/")
+    @PostMapping("/add")
     public void addStories (@Valid @RequestBody StoriesRequestDto storiesRequestDto) {
 
         log.info("Received json with StoriesRequestDto, which contains: {} ", storiesRequestDto);
 
-        storiesService.saveJson(storiesRequestDto);
+        storiesService.saveFiles(storiesRequestDto);
     }
 }
--- backend/src/main/java/ru/cft/shiftlab/contentmaker/services/IStoriesService.java
+++
@@ -1,8 +0,0 @@
-package ru.cft.shiftlab.contentmaker.services;
-
-import ru.cft.shiftlab.contentmaker.dto.StoriesRequestDto;
-
-public interface IStoriesService {
-
-    void saveJson(StoriesRequestDto storiesRequestDto);
-}
