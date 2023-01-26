2bc2b13f828375927aef33a408213ae8f255cfab
alyoneek <alyonta03@mail.ru>
2023-01-27 03:27:56 +0700

    Добавила стили ко всем компонентам

--- frontend/src/components/CustomForm.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const CustomForm = ({ a }) => {
-  return <div>CustomForm</div>;
-};
-
-export default CustomForm;
--- frontend/src/components/FormField/index.jsx
+++ frontend/src/components/FormField/index.jsx
@@ -1,18 +1,49 @@
 import React from 'react';
 import { Field, ErrorMessage } from 'formik';
 
-const FormField = ({ labelTitle, name, type, component, as, options }) => {
+import formFieldStyles from './FormField.module.scss';
+
+const FormField = ({
+  labelTitle,
+  name,
+  type,
+  value,
+  checked,
+  component,
+  as,
+  options,
+  handleClick,
+}) => {
   return (
-    <div>
-      <label htmlFor={name}>{labelTitle}</label>
-      <Field name={name} placeholder="" type={type} component={component} as={as}>
-        {options &&
-          options.map((option, i) => (
-            <option key={i} value={option.value}>
-              {option.name}
-            </option>
-          ))}
-      </Field>
+    <div className={formFieldStyles.root}>
+      {type === 'radio' ? (
+        <>
+          <label htmlFor={name} className={formFieldStyles.radio_title}>
+            {labelTitle}
+          </label>
+          <Field name={name} type={type} value={value} onChange={handleClick} checked={checked} />
+        </>
+      ) : (
+        <>
+          <label htmlFor={name} className={formFieldStyles.label_title}>
+            {labelTitle}
+          </label>
+          <Field
+            name={name}
+            placeholder=""
+            type={type}
+            component={component}
+            as={as}
+            className={formFieldStyles.field}>
+            {options &&
+              options.map((option, i) => (
+                <option key={i} value={option.value}>
+                  {option.name}
+                </option>
+              ))}
+          </Field>
+        </>
+      )}
       <ErrorMessage name={name} component="div" className="field-error" />
     </div>
   );
