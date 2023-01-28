322255bae756a4f95022f453d299763813676376
iseeyouwatching <leonidshibanovv@gmail.com>
2023-01-28 22:44:04 +0700

    Рефакторинг кода в методе сервиса JsonAndImageSaverService
    - удалил неиспользуемые в данный момент байтовые массивы previewUrl и storyFramePictureUrl;
    - перенес повторяющийся код в метод класса ByteArrayToImageConverter

---
+++ backend/src/main/java/ru/cft/shiftlab/contentmaker/util/ByteArrayToImageConverter.java
@@ -0,0 +1,19 @@
+package ru.cft.shiftlab.contentmaker.util;
+
+import org.springframework.stereotype.Component;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.IOException;
+
+public class ByteArrayToImageConverter {
+
+    public void convertByteArrayToImageAndSave(byte [] bytes, String directory, String fileName, String fileFormat, int counter) throws IOException {
+        ByteArrayInputStream bis = new ByteArrayInputStream(bytes);
+        BufferedImage bImage2 = ImageIO.read(bis);
+        ImageIO.write(bImage2, "png", new File(directory, fileName + counter + fileFormat));
+    }
+
+}
