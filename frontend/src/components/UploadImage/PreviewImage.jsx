e0549eaa4f262946b8291966074f4be593c4c62d
alyoneek <alyonta03@mail.ru>
2023-02-02 23:37:43 +0700

    Реализовала возможность удалять фото

--- frontend/src/components/UploadImage/PreviewImage.jsx
+++ frontend/src/components/UploadImage/PreviewImage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const PreviewImage = ({ image }) => {
+const PreviewImage = ({ image, setImage, setFieldValue, fieldName, input }) => {
   const [preview, setPreview] = React.useState(null);
 
   const reader = new FileReader();
@@ -9,9 +9,23 @@ const PreviewImage = ({ image }) => {
     setPreview(reader.result);
   };
 
+  const handleDeleteImage = () => {
+    input.value = '';
+    setPreview(null);
+    setImage(null);
+    setFieldValue(fieldName, null);
+  };
+
   return (
     <div style={{ marginTop: '10px' }}>
-      {preview ? <img alt="preview" src={preview} width="100px" height="100px" /> : 'Loading...'}
+      {preview ? (
+        <>
+          <img alt="preview" src={preview} width="100px" height="100px" />
+          <button onClick={handleDeleteImage}>Удалить</button>
+        </>
+      ) : (
+        'Loading...'
+      )}
     </div>
   );
 };
--- frontend/src/components/UploadImage/index.jsx
+++ frontend/src/components/UploadImage/index.jsx
@@ -4,10 +4,12 @@ import PreviewImage from './PreviewImage';
 
 const UploadImage = ({ field, form, ...props }) => {
   const [image, setImage] = React.useState(null);
+  const inputRef = React.useRef();
 
   const handleUploadImage = async (e) => {
     let img = e.target.files[0];
     setImage(img);
+
     if (img) {
       const buffer = await img.arrayBuffer();
       let byteArray = new Uint8Array(buffer);
@@ -20,12 +22,21 @@ const UploadImage = ({ field, form, ...props }) => {
   return (
     <>
       <input
+        ref={inputRef}
         name={field.name}
         type="file"
         accept="image/*"
         onChange={(e) => handleUploadImage(e)}
       />
-      {image && <PreviewImage image={image} />}
+      {image && (
+        <PreviewImage
+          image={image}
+          setImage={setImage}
+          setFieldValue={form.setFieldValue}
+          fieldName={field.name}
+          input={inputRef.current}
+        />
+      )}
     </>
   );
 };
