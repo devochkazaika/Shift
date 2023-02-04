abb33d2b531cdc2eddc837834264cab396228708
iseeyouwatching <leonidshibanovv@gmail.com>
2023-02-04 20:21:47 +0700

    Добавил javadoc для вспомогательного класса FileNameCreator

--- backend/src/main/java/ru/cft/shiftlab/contentmaker/util/FileNameCreator.java
+++ backend/src/main/java/ru/cft/shiftlab/contentmaker/util/FileNameCreator.java
@@ -2,9 +2,17 @@ package ru.cft.shiftlab.contentmaker.util;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
-
+/**
+ * Класс, предназначенный для генерации названия JSON файла.
+ */
 public class FileNameCreator {
 
+    /**
+     * Метод, который генерирует название JSON файла.
+     *
+     * @param previewTitle заголовок истории (в будущем будет заменено на bankId).
+     * @return название JSON файла.
+     */
     public static String createFileName(String previewTitle) {
         Date date = new Date();
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
