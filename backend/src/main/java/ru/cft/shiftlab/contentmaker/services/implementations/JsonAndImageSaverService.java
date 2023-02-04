098a12af4f66b556438bd5459e04061f12374d19
iseeyouwatching <leonidshibanovv@gmail.com>
2023-02-04 19:59:47 +0700

    Добавил конвертацию StoriesRequestDto в StoryPresentation в метод сервиса

--- backend/src/main/java/ru/cft/shiftlab/contentmaker/services/implementations/JsonAndImageSaverService.java
+++ backend/src/main/java/ru/cft/shiftlab/contentmaker/services/implementations/JsonAndImageSaverService.java
@@ -2,16 +2,21 @@ package ru.cft.shiftlab.contentmaker.services.implementations;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 import ru.cft.shiftlab.contentmaker.dto.StoriesRequestDto;
 import ru.cft.shiftlab.contentmaker.dto.StoryDto;
 import ru.cft.shiftlab.contentmaker.dto.StoryFramesDto;
+import ru.cft.shiftlab.contentmaker.entity.StoryPresentation;
 import ru.cft.shiftlab.contentmaker.services.FileSaverService;
 import ru.cft.shiftlab.contentmaker.util.ByteArrayToImageConverter;
 import ru.cft.shiftlab.contentmaker.util.FileExtensionExtractor;
+import ru.cft.shiftlab.contentmaker.util.RequestDtoToEntityConverter;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -24,7 +29,12 @@ public class JsonAndImageSaverService implements FileSaverService {
             String jsonDirectory =
                     "/content-maker/backend/src/main/resources/site/share/htdoc/_files/skins/mobws_story";
             String fileName = "stories.json";
-            mapper.writeValue(new File(jsonDirectory, fileName), storiesRequestDto);
+            List<StoryPresentation> presentationList = new ArrayList<>();
+            RequestDtoToEntityConverter requestDtoToEntityConverter = new RequestDtoToEntityConverter(new ModelMapper());
+            for (StoryDto storyDto: storiesRequestDto.getStoryDtos()) {
+                presentationList.add(requestDtoToEntityConverter.fromStoriesRequestDtoToStoryPresentation(storyDto));
+            }
+            mapper.writeValue(new File(jsonDirectory, fileName), presentationList);
             int counterForPreview = 0;
             int counterForStoryFramePicture = 0;
             String picturesDirectory =
