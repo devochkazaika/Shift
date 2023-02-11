fdf0e2faabc6a22f08db456b2ecebe96d58b9903
alyoneek <alyonta03@mail.ru>
2023-02-11 19:44:36 +0700

    Вынесла иконки в assets

--- frontend/src/components/Modal/index.jsx
+++ frontend/src/components/Modal/index.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { disableBodyScroll, enableBodyScroll } from 'body-scroll-lock';
 import { motion } from 'framer-motion';
 
+import { ReactComponent as CloseIcon } from '../../assets/icons/close.svg';
+
 import modalStyles from './Modal.module.scss';
 
 const Modal = ({ onClose, image }) => {
@@ -23,14 +25,7 @@ const Modal = ({ onClose, image }) => {
   return (
     <div className={modalStyles.overlay} onClick={onBackgroundClick} ref={modalRef}>
       <div>
-        <svg
-          onClick={closeModal}
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-          width="50px"
-          className={modalStyles.btn_close}>
-          <path d="M4.293,18.293,10.586,12,4.293,5.707A1,1,0,0,1,5.707,4.293L12,10.586l6.293-6.293a1,1,0,1,1,1.414,1.414L13.414,12l6.293,6.293a1,1,0,1,1-1.414,1.414L12,13.414,5.707,19.707a1,1,0,0,1-1.414-1.414Z" />
-        </svg>
+        <CloseIcon className={modalStyles.btn_close} onClick={closeModal} width="50px" />
         <motion.div
           initial={{ scale: 0.2, x: '-80%', opacity: 0 }}
           animate={{ scale: 1, x: 0, opacity: 1 }}
--- frontend/src/components/UploadImage/PreviewImage.jsx
+++ frontend/src/components/UploadImage/PreviewImage.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { AnimatePresence } from 'framer-motion';
 
+import { ReactComponent as BinIcon } from '../../assets/icons/bin.svg';
+
 import previewImageStyles from './PreviewImage.module.scss';
 import { convertByteArrayToSrc } from '../../utils/helpers/byteArrayFunctions';
 import Modal from '../Modal';
@@ -32,15 +34,7 @@ const PreviewImage = ({ image, setFieldValue, fieldName, input }) => {
             onClick={() => toggleModal(true)}
           />
           <div className={previewImageStyles.delete} onClick={handleDeleteImage}>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fillRule="evenodd"
-              strokeLinejoin="round"
-              strokeMiterlimit="2"
-              clipRule="evenodd"
-              viewBox="0 0 32 32">
-              <path d="M21 5V4a3 3 0 0 0-3-3h-4a3 3 0 0 0-3 3v1H4a1 1 0 0 0 0 2h1v20.91c0 .796.316 1.559.879 2.121A2.996 2.996 0 0 0 8 30.91h16c.796 0 1.559-.316 2.121-.879A2.996 2.996 0 0 0 27 27.91V7h1a1 1 0 0 0 0-2h-7Zm4 2v20.91a.997.997 0 0 1-1 1H8a.997.997 0 0 1-1-1V7h18Zm-6-2h-6V4a1 1 0 0 1 1-1h4a1 1 0 0 1 1 1v1Z" />
-            </svg>
+            <BinIcon />
             <span>Удалить</span>
           </div>
           <AnimatePresence initial={false} onExitComplete={() => null}>
