6b1801a79a29258abca1584b6ca884c3b039e994
alyoneek <alyonta03@mail.ru>
2023-03-01 03:14:31 +0700

    Добавила валидацию для изображений
    - Изменила хранимое в values значение image на file вместо byteArray
    - Добавила 3 валидации (формат, стороны, размер)
    - Добавила константы с максимально допустимыми значениями для
      изображения
    - Добавила функцию конвертации values в новый объект payload, у которого
      значения полей изображений будут byteArray

--- frontend/src/utils/constants/validation.js
+++ frontend/src/utils/constants/validation.js
@@ -12,3 +12,8 @@ export const previewRules = [
     maxTextLength: 140,
   },
 ];
+
+export const MAX_IMAGE_SIZE = 500;
+export const SUPPORTED_FORMATS = ['image/jpg', 'image/jpeg', 'image/png'];
+export const MAX_IMAGE_WIDTH = 1333;
+export const MAX_IMAGE_HEIGHT = 2000;
