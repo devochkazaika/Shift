20fa7e0b50a2351eb5bca9abe93903967f5c8371
Buhancev <maxim.buhancev@gmail.com>
2023-03-13 19:10:09 +0700

    Перенес проект из Maven в Gradle, но что-то не работает:(

--- backend/src/main/java/ru/cft/shiftlab/contentmaker/controller/StoriesController.java
+++ backend/src/main/java/ru/cft/shiftlab/contentmaker/controller/StoriesController.java
@@ -1,7 +1,6 @@
 package ru.cft.shiftlab.contentmaker.controller;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import ru.cft.shiftlab.contentmaker.dto.StoriesRequestDto;
