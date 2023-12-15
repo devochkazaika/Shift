e44d390f826b0064d1acc7ba347c761446722b14
hot-play <usov2002@yandex.ru>
2023-12-15 17:39:17 +0700

    SHIFTLAB-6063 Сделал обработку ошибок
    Добавил тосты с отображением ошибок от бэка

--- frontend/src/api/stories.js
+++ frontend/src/api/stories.js
@@ -1,13 +1,36 @@
 import axios from "axios";
+import { defaultToastOptions, defaultUpdateToastOptions } from '../utils/constants/toastOptions';
+import { toast } from "react-toastify";
 
 export const uploadStories = async (jsonPayload) => {
-  // TODO: Информативный вывод и проверка по статусам ответа
+  const toastView = toast("Отправляем данные", {
+    ...defaultToastOptions,
+    autoClose: false,
+    isLoading: true,
+  });
   try {
-    await axios.post("/stories/add", jsonPayload, {
+    const response = await axios.post("/stories/add", jsonPayload, {
       headers: { "Content-Type": "application/json" },
+    });
+    toast.update(toastView, {
+      ...defaultUpdateToastOptions,
+      render: response.message,
     });
     return true;
   } catch (error) {
+    if (error.response.status === 500) {
+      toast.update(toastView, {
+        ...defaultUpdateToastOptions,
+        render: "Ошибка соединения с сервером",
+        type: "warning"
+      });
+      return false;
+    }
+    toast.update(toastView, {
+      ...defaultUpdateToastOptions,
+      render: error.message,
+      type: "warning"
+    });
     return false;
   }
 };
