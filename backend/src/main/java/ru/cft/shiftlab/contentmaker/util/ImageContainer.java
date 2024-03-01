8eba9a6917ea12b4c7aadf98ea14e0e673ac7b22
Andrew <a.zolotorevskii@g.nsu.ru>
2024-03-01 12:06:33 +0700

    класс для перебора картинок добавлен

---
+++ backend/src/main/java/ru/cft/shiftlab/contentmaker/util/ImageContainer.java
@@ -0,0 +1,28 @@
+package ru.cft.shiftlab.contentmaker.util;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.web.multipart.MultipartFile;
+
+
+/**
+ * Сервис предназначенный для для перебора картинок по порядку.
+ */
+@Setter
+@Getter
+public class ImageContainer {
+    MultipartFile[] images;
+    Integer counterImages = 0;
+
+    public ImageContainer(MultipartFile[] images) {
+        this.images = images;
+    }
+
+    public MultipartFile getNextImage(){
+        return images[counterImages++];
+    }
+
+    public void decrementCounterImages(){
+        counterImages--;
+    }
+}
