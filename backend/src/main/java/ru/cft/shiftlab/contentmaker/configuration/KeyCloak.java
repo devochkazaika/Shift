40e74460d11e43596fe0378b85c75a499a99b426
Panarin Ilya <cccthafcdh@gmail.com>
2024-05-22 18:21:21 +0700

    add keycloak

--- backend/src/main/java/ru/cft/shiftlab/contentmaker/configuration/KeyCloak.java
+++ backend/src/main/java/ru/cft/shiftlab/contentmaker/configuration/KeyCloak.java
@@ -1,15 +1,21 @@
 package ru.cft.shiftlab.contentmaker.configuration;
 
+import net.bytebuddy.dynamic.TypeResolutionStrategy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
+import java.security.interfaces.RSAPublicKey;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -19,28 +25,52 @@ import java.util.stream.Stream;
 public class KeyCloak {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
-        http.oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
+//        http.oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
         return http
-                .authorizeHttpRequests(o ->
-                        o.anyRequest().hasRole("ROLE_ADMIN")
-                )
-        .build();
+                .authorizeHttpRequests((authorize) -> authorize.anyRequest().authenticated())
+                .sessionManagement(configurer -> configurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .csrf(CsrfConfigurer::disable)
+                .oauth2ResourceServer(configurer -> configurer.jwt(jwt ->{
+                    JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+                    jwtAuthenticationConverter.setPrincipalClaimName("preferred_username");
+                    jwt.jwtAuthenticationConverter(jwtAuthenticationConverter);
+
+                    JwtGrantedAuthoritiesConverter jwtGrantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+
+                    JwtGrantedAuthoritiesConverter customJwtGrantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+                    customJwtGrantedAuthoritiesConverter.setAuthoritiesClaimName("groups");
+                    customJwtGrantedAuthoritiesConverter.setAuthorityPrefix("");
+
+                    jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(token ->
+                            Stream.concat(jwtGrantedAuthoritiesConverter.convert(token).stream(),
+                                            customJwtGrantedAuthoritiesConverter.convert(token).stream())
+                                    .collect(Collectors.toList()));
+                }))
+                .build();
     }
-    @Bean
-    public JwtAuthenticationConverter jwtAuthenticationConverter(){
-        var converter = new JwtAuthenticationConverter();
-        var jwtGrantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
-        converter.setPrincipalClaimName("preferred_username");
-        converter.setJwtGrantedAuthoritiesConverter(
-                jwt ->{
-                    var authorities = jwtGrantedAuthoritiesConverter.convert(jwt);
-                    var roles = (List<String>) jwt.getClaimAsMap("realm_access").get("roles");
-                    return Stream.concat(authorities.stream(),
-                            roles.stream()
-                                    .filter(role -> role.startsWith("ROLE_"))
-                                    .map(GrantedAuthority.class::cast)).collect(Collectors.toList());
-                }
-        );
-        return converter;
-    }
+//    @Bean
+//    public JwtAuthenticationConverter jwtAuthenticationConverter(){
+//        var converter = new JwtAuthenticationConverter();
+//        var jwtGrantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+//        converter.setPrincipalClaimName("preferred_username");
+//        converter.setJwtGrantedAuthoritiesConverter(
+//                jwt ->{
+//                    var authorities = jwtGrantedAuthoritiesConverter.convert(jwt);
+//                    var roles = (List<String>) jwt.getClaimAsMap("realm_access").get("roles");
+//                    return Stream.concat(authorities.stream(),
+//                            roles.stream()
+//                                    .filter(role -> role.startsWith("ROLE_"))
+//                                    .map(GrantedAuthority.class::cast)).collect(Collectors.toList());
+//                }
+//        );
+//        return converter;
+//    }
+//    @Bean
+//    public JwtDecoder jwtDecoder() {
+//        return NimbusJwtDecoder.withPublicKey(publicKey()).build();
+//    }
+//
+//    private RSAPublicKey publicKey() {
+//        // ...
+//    }
 }
---
+++ backend/src/main/java/ru/cft/shiftlab/contentmaker/controller/AuthenticateController.java
@@ -0,0 +1,10 @@
+package ru.cft.shiftlab.contentmaker.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/authenticate")
+public class AuthenticateController {
+
+}
