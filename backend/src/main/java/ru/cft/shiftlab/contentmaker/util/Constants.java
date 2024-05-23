8aca61c87e1cf1f1bd3090cf4938c3c145c649cb
Panarin Ilya <cccthafcdh@gmail.com>
2024-05-23 22:33:36 +0700

    поменял savefiles, теперь они сохраняются в resources

--- backend/src/main/java/ru/cft/shiftlab/contentmaker/util/Constants.java
+++ backend/src/main/java/ru/cft/shiftlab/contentmaker/util/Constants.java
@@ -3,5 +3,5 @@ package ru.cft.shiftlab.contentmaker.util;
 public class Constants {
     public static final String STORIES = "stories";
     public static final String FILES_SAVE_DIRECTORY =
-            "/content-maker/backend/src/main/resources/site/share/htdoc/_files/skins/mobws_story/";
+            "backend/src/main/resources/site/share/htdoc/_files/skins/mobws_story/";
 }
