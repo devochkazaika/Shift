a8b457d053a57363598dad490770bcec4bd07b61
Panarin Ilya <cccthafcdh@gmail.com>
2024-05-26 19:26:17 +0700

    для develop

--- backend/src/main/java/ru/cft/shiftlab/contentmaker/util/Constants.java
+++ backend/src/main/java/ru/cft/shiftlab/contentmaker/util/Constants.java
@@ -5,7 +5,8 @@ public class Constants {
     public static final String FILES_SAVE_DIRECTORY =
 //            System.getProperty("user.dir").endsWith("backend") ?
 //            System.getProperty("user.dir") + "/src/main/resources/site/share/htdoc/_files/skins/mobws_story/" :
-            System.getProperty("user.dir") + "/backend/src/main/resources/site/share/htdoc/_files/skins/mobws_story/" ;
+//            System.getProperty("user.dir") + "/backend/src/main/resources/site/share/htdoc/_files/skins/mobws_story/" ;
+    "/content-maker/backend/src/main/resources/site/share/htdoc/_files/skins/mobws_story/";
 
     public static final String FILES_TEST_DIRECTORY =
             System.getProperty("user.dir") + "/src/test/java/ru/cft/shiftlab/contentmaker/test_pictures/";
