746cd325a819f38fa2ce28fca37a8cb9350fd253
Panarin Ilya <cccthafcdh@gmail.com>
2024-05-26 17:48:47 +0700

    тест для удаления json

--- backend/src/main/java/ru/cft/shiftlab/contentmaker/util/Constants.java
+++ backend/src/main/java/ru/cft/shiftlab/contentmaker/util/Constants.java
@@ -3,7 +3,7 @@ package ru.cft.shiftlab.contentmaker.util;
 public class Constants {
     public static final String STORIES = "stories";
     public static final String FILES_SAVE_DIRECTORY =
-            "/src/main/resources/site/share/htdoc/_files/skins/mobws_story/";
+            System.getProperty("user.dir") +"/src/main/resources/site/share/htdoc/_files/skins/mobws_story/";
     public static final String FILES_SAVETEST_DIRECTORY =
             System.getProperty("user.dir") + "/src/main/resources/site/share/htdoc/_files/skins/mobws_story/";
     public static final String FILES_TEST_DIRECTORY =
--- backend/src/test/java/ru/cft/shiftlab/contentmaker/Stories/DeleteRequestTest.java
+++ backend/src/test/java/ru/cft/shiftlab/contentmaker/Stories/DeleteRequestTest.java
@@ -1,5 +1,7 @@
 package ru.cft.shiftlab.contentmaker.Stories;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.commons.io.IOUtils;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -8,6 +10,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.modelmapper.ModelMapper;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.web.multipart.MultipartFile;
+import ru.cft.shiftlab.contentmaker.entity.StoryPresentation;
 import ru.cft.shiftlab.contentmaker.service.implementation.JsonProcessorService;
 import ru.cft.shiftlab.contentmaker.util.DtoToEntityConverter;
 import ru.cft.shiftlab.contentmaker.util.FileNameCreator;
@@ -23,11 +26,18 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Stream;
 
-import static ru.cft.shiftlab.contentmaker.util.Constants.*;
+import static ru.cft.shiftlab.contentmaker.util.Constants.FILES_SAVE_DIRECTORY;
+import static ru.cft.shiftlab.contentmaker.util.Constants.FILES_TEST_DIRECTORY;
 
 @ExtendWith(MockitoExtension.class)
 public class DeleteRequestTest {
+    ObjectMapper objectMapper = new ObjectMapper();
     private final MultipartFileToImageConverter multipartFileToImageConverter = new MultipartFileToImageConverter(new ImageNameGenerator());
     private final DtoToEntityConverter dtoToEntityConverter = new DtoToEntityConverter(new ModelMapper());
     @Test
@@ -64,7 +74,28 @@ public class DeleteRequestTest {
         File file = new File(saveDirectory);
         File[] files = file.listFiles();
         Assertions.assertEquals(0, files.length);
+    }
+    @Test
+    void deleteJsonTest() throws NoSuchMethodException, IOException, InvocationTargetException, IllegalAccessException {
+        Method method = JsonProcessorService.class.getDeclaredMethod("deleteJsonStories", String.class, String.class, String.class);
+        File jsonFile = new File(FILES_TEST_DIRECTORY + "/story_tkbbank_web.json");
+        copyFile(jsonFile.getAbsolutePath(), FILES_SAVE_DIRECTORY+"/story_tkbbank_web.json");
+        File json = new File(FILES_SAVE_DIRECTORY+"/story_tkbbank_web.json");
+        JsonProcessorService service = new JsonProcessorService(multipartFileToImageConverter,
+                dtoToEntityConverter);
 
+        method.invoke(service, "tkbbank", "WEB", "0");
+
+        StringBuilder jsonStr = new StringBuilder();
+        Files.readAllLines(json.toPath()).forEach(jsonStr::append);
+        Map<String, List<StoryPresentation>> map = objectMapper.readValue(
+                jsonStr.toString(),
+                new TypeReference<Map<String, List<StoryPresentation>>>() {}
+        );
+        Set<Long> set = new HashSet<>();
+        Stream.of(map.get("stories"))
+                .forEach(x -> set.add(x.get(0).getId()));
+        Assertions.assertTrue(!set.contains(0L));
     }
     public static void copyFile(String sourcePathStr, String destinationPathStr) {
         // Преобразуем строки в Path объекты
