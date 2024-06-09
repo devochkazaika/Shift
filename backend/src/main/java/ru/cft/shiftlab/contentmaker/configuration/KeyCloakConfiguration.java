efad403131ebb4af788d53ad61bc1e80467350cd
Panarin Ilya <cccthafcdh@gmail.com>
2024-06-09 14:34:14 +0700

    поменял зависимости

--- backend/src/main/java/ru/cft/shiftlab/contentmaker/configuration/KeyCloakConfiguration.java
+++ backend/src/main/java/ru/cft/shiftlab/contentmaker/configuration/KeyCloakConfiguration.java
@@ -8,9 +8,18 @@ import org.keycloak.admin.client.KeycloakBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Configuration
 @EnableWebSecurity
@@ -31,7 +40,7 @@ public class KeyCloakConfiguration {
     }
 
     @Bean
-    public Keycloak getAdminKeycloakUser() {
+    public Keycloak createBeanKeycloak() {
         var t = KeycloakBuilder.builder()
                 .serverUrl("http://localhost:8081")
                 .grantType(OAuth2Constants.PASSWORD)
@@ -45,10 +54,30 @@ public class KeyCloakConfiguration {
     }
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http.oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
+//        http.oauth2Login(Customizer.withDefaults());
         return http.csrf().disable().authorizeHttpRequests(authorizaHttpRequest -> authorizaHttpRequest
+                        .antMatchers("/createUser").hasRole("ADMIN")
                         .anyRequest().permitAll()
 
 //                        .antMatchers("/createUser").permitAll())
         ).build();
     }
+    @Bean
+    public JwtAuthenticationConverter customJwtAuthenticationConverter(){
+        var converter = new JwtAuthenticationConverter();
+        var jwtGrantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+        converter.setPrincipalClaimName("preferred_username");
+        converter.setJwtGrantedAuthoritiesConverter(jwt -> {
+            var convert = jwtGrantedAuthoritiesConverter.convert(jwt);
+            var roles = (List<String>) jwt.getClaimAsMap("realm_access").get("roles");
+            return Stream.concat(convert.stream(),
+                            roles.stream()
+                                    .map(role -> new SimpleGrantedAuthority("ROLE_" + role))
+                                    .map(GrantedAuthority.class::cast))
+                    .collect(Collectors.toList());
+        });
+        return converter;
+    }
+
 }
--- backend/src/test/java/ru/cft/shiftlab/contentmaker/Keycloak/KeycloakStartTest.java
+++ backend/src/test/java/ru/cft/shiftlab/contentmaker/Keycloak/KeycloakStartTest.java
@@ -1,22 +1,36 @@
 package ru.cft.shiftlab.contentmaker.Keycloak;
 
 import lombok.RequiredArgsConstructor;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.keycloak.admin.client.Keycloak;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import ru.cft.shiftlab.contentmaker.controller.StoriesController;
+import org.springframework.test.web.servlet.MockMvc;
+import ru.cft.shiftlab.contentmaker.controller.HelloWorldController;
+import ru.cft.shiftlab.contentmaker.service.KeycloakService;
 
 @ExtendWith(SpringExtension.class)
-@WebMvcTest(value = StoriesController.class)
+@WebMvcTest(value = HelloWorldController.class)
 @RequiredArgsConstructor
 public class KeycloakStartTest {
     @MockBean
-    private final Keycloak keycloak;
-    @Test
-    public void createBeanTest(){
-        keycloak.realm("content-maker").getClientSessionStats();
-    }
+    private Keycloak keycloak;
+    @MockBean
+    private KeycloakService keycloakService;
+    @Autowired
+    private MockMvc mockMvc;
+//    @Test
+//    public void createBeanTest() throws Exception {
+//        Mockito.when(keycloakService.getRealm()).thenCallRealMethod();
+//        Mockito.when(keycloakService.getRealm().getClientSessionStats()).thenCallRealMethod();
+//        mockMvc.perform(MockMvcRequestBuilders
+//                        .get("/check-keycloak")
+//                        .contentType(MediaType.APPLICATION_JSON))
+//                .andExpect(status().isOk()
+//                );
+//    }
+
+
 }
