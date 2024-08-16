9f5715310547496a8f4633b56499d538f8858f45
Ilya Panarin <you@example.com>
2024-08-16 13:45:23 +0300

    починил сохранения

--- backend/src/main/java/ru/cft/shiftlab/contentmaker/util/Constants.java
+++ backend/src/main/java/ru/cft/shiftlab/contentmaker/util/Constants.java
@@ -5,7 +5,7 @@ import java.nio.file.Paths;
 public class Constants {
     public static final String STORIES = "stories";
     public static final String FILES_SAVE_DIRECTORY =
-            String.valueOf(Paths.get("backend/src/main/resources/static").toAbsolutePath())+"/site/share/htdoc/_files/skins/mobws_story/";
+            String.valueOf(Paths.get("src/main/resources/static").toAbsolutePath())+"/site/share/htdoc/_files/skins/mobws_story/";
 //            System.getProperty("user.dir").endsWith("backend") ?
 //            System.getProperty("user.dir") + "/src/main/resources/site/share/htdoc/_files/skins/mobws_story/" :
 //            System.getProperty("user.dir") + "/backend/src/main/resources/site/share/htdoc/_files/skins/mobws_story/" ;
