b6c5ab8da4889163111d25c5884b7474f6954b82
alice <elizabethfox434@gmail.com>
2024-08-21 23:02:58 +0700

    SHIFTLAB-7567 добавила пункты в раздел сайта

--- frontend/src/components/Banners/WebsiteSectionBanner.jsx
+++ frontend/src/components/Banners/WebsiteSectionBanner.jsx
@@ -1,29 +1,39 @@
+/* eslint-disable */
 import React from "react";
 import FormField from "../FormField";
+import { webSectionsOptions } from "../../utils/constants/webSectionsOptions";
 
 const WebsiteSectionBanner = () => {
   return (
     <div>
-         <div className="row"><label htmlFor={`ButtonIntarectiveType`}>
-         Всем пользователям
+      <div className="input_field">
+        <FormField name={`gradient`} as="select" options={webSectionsOptions} />
+      </div>
+      <div className="input_field">
         <FormField
+          labelTitle="Приоритет"
+          name={`stories.buttonUrl`}
+          type="text"
+        />
+      </div>
+      <div className="row">
+        <FormField
+          labelTitle="Всем пользователям"
           name={`visibleButtonOrNone`}
           value="BUTTON"
           id={`ButtonIntarectiveType`}
-          type="radio"
+          type="checkbox"
         />
-      </label></div>
-      <div className="row"><label htmlFor={`ButtonIntarectiveType`}>
-         Активен
+      </div>
+      <div className="row">
         <FormField
+          labelTitle="Активен"
           name={`visibleButtonOrNone`}
           value="BUTTON"
           id={`ButtonIntarectiveType`}
-          type="radio"
+          type="checkbox"
         />
-      </label></div>
-      
-
+      </div>
     </div>
   );
 };
---
+++ frontend/src/utils/constants/webSectionsOptions.js
@@ -0,0 +1,4 @@
+export const webSectionsOptions = [
+    { value: 'EMPTY', name: 'МП. Главная страница' },
+  ];
+  
