d052c294940111a44755a60805f9ec80bd8b6e60
Ilya Panarin <cccthafcdh@gmail.com>
2024-10-06 18:34:28 +0300

    Починка юнит тестов для банеров

---
+++ backend/src/test/resources/db.migration/V3__create_history_table.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS history
+(
+    id SERIAL PRIMARY KEY NOT NULL,
+    component_type varchar(30),
+    operation_type varchar(30),
+    bank varchar(30),
+    platform varchar(30),
+    status varchar(30),
+    component_id BIGINT,
+    "day" date,
+    time time,
+    user_name varchar(255)
+);
